563e4d398dd93b021052ae34dbe00d700e467aaf
name <you@example.com>
2023-08-02 19:08:00 +0800

    -

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
 import 'normalize.css'
 import '../styles/styles.scss'
 
@@ -6,8 +7,23 @@ const IndexPage = () => {
   return (
     <main>
         <div className='headBanner'>
+            <div className='banner'>
+                <StaticImage src='../images/undraw_learning_sketching_nd4f.svg' style={{ width: '300px' }} />
+                <div>Smart manage <br/> time with AI</div>
+            </div>
+            <div className='navBar'>
+                <div>
+                    <a href='#'>Logo</a>
+                </div>
+                <div className='btns'>
+                    <a href='#'>Blog</a>
+                    <a href='#'>Contact</a>
+                    <a href='#'>About</a>
+                </div>
+                <div></div>
+            </div>
             <div className='upButton'>
-                <span>Download</span>
+                <a href='#'>Download</a>
                 <div className='round1'><div/></div>
                 <div className='round2'><div/></div>
             </div>
