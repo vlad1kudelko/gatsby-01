acb41c0e7e02b9ebec2325fdd37e927628b6910c
name <you@example.com>
2023-08-04 14:47:21 +0800

    tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,66 +5,79 @@ import '../styles/styles.scss'
 
 const IndexPage = () => {
   return (
-    <div className='page'>
+    <div className='max-w-screen-lg mx-auto'>
         <header>
-            <div className='headBanner'>
-                <div className='banner'>
+            <div className='headBanner relative overflow-hidden'>
+                <div className='absolute pr-2 top-0 right-0 left-0 bottom-0 flex items-center justify-center'>
                     <StaticImage
                         alt=''
                         placeholder='blurred'
                         src='../images/man.png'
-                        className='img'
+                        className='hidden md:block self-end'
                     />
-                    <div className='title'>Smart 📈 manage <br/> time with AI</div>
-                </div>
-                <div className='navBar'>
-                    <div>
-                        <a href='#Logo'>Logo</a>
+                    <div className='text-5xl text-center' style={{fontFamily: 'Ubuntu, sans-serif'}}>
+                        Smart 📈 manage <br/> time with AI
                     </div>
-                    <div className='btns'>
-                        <a href='#Blog'>Blog</a>
-                        <a href='#Contact'>Contact</a>
-                        <a href='#About'>About</a>
+                </div>
+                <div className='absolute top-0 right-0 left-0 text-lg md:flex md:justify-between md:py-5 md:px-8'>
+                    <div className='py-5 px-8 md:p-0'>
+                        <a className='hover:text-gray-600' href='#Logo'>Logo</a>
+                    </div>
+                    <div className='py-5 px-8 md:p-0 text-center'>
+                        <a className='p-5 hover:text-gray-600' href='#Blog'>Blog</a>
+                        <a className='p-5 hover:text-gray-600' href='#Contact'>Contact</a>
+                        <a className='p-5 hover:text-gray-600' href='#About'>About</a>
                     </div>
                     <div></div>
                 </div>
-                <div className='upButton'>
-                    <a href='#Download'>Download</a>
-                    <div className='round1'><div/></div>
-                    <div className='round2'><div/></div>
+                <div className='upButton absolute bg-black top-0 right-0 text-lg uppercase font-bold py-5 px-8'>
+                    <a className='text-yellow-300' href='#Download'>Download</a>
+                    <div className='round1 absolute'>
+                        <div className='absolute top-0 right-0 left-0 bottom-0'/>
+                    </div>
+                    <div className='round2 absolute'>
+                        <div className='absolute top-0 right-0 left-0 bottom-0'/>
+                    </div>
                 </div>
             </div>
-            <div className='subBanners'>
-                <div className='box1'>
-                    <div className='gridBox'>
-                        <div className='nameBox'> [rating] </div>
-                        <div className='ratingNumber'>
-                            <span>4.9</span>
+            <div className='subBanners grid grid-cols-2 md:grid-cols-3'>
+                <div className='box1 text-white'>
+                    <div className='gridBox grid grid-cols-1 gap-2.5 md:grid-cols-2 md:gap-0'>
+                        <div className='text-xs uppercase'> [rating] </div>
+                        <div className='text-3xl md:text-6xl text-center'>
+                            <span className='relative'>
+                                4.9
+                                <span className='absolute text-xl'>⭐</span>
+                            </span>
                         </div>
                     </div>
-                    <div className='avaList'>
+                    <div className='avaList flex'>
                         <StaticImage 
                             alt=''
                             placeholder='blurred'
                             src='../images/thispersondoesnotexist.jpg'
-                            className='avatar'/>
-                        <div className='avatar'></div>
+                            className='avatar h-12 w-12 rounded-full'/>
+                        <div className='avatar h-12 w-12 rounded-full bg-yellow-300'></div>
                     </div>
                 </div>
-                <div className='box2'>
+                <div className='box2 hidden md:block text-lg font-bold'>
                     <div>Use the time to your own advantage every day</div>
                 </div>
-                <div className='box3'>
+                <div className='box3 relative'>
                     <div className='border1'>
-                        <div></div>
-                        <div></div>
+                        <div className='absolute'></div>
+                        <div className='absolute'></div>
                     </div>
-                    <div className='round1'><div/></div>
-                    <div className='round2'><div/></div>
-                    <div className='contentBox'>
-                        <div className='nameBox'> [tasks done] </div>
+                    <div className='round1 absolute'>
+                        <div className='absolute top-0 right-0 left-0 bottom-0'/>
+                    </div>
+                    <div className='round2 absolute'>
+                        <div className='absolute top-0 right-0 left-0 bottom-0'/>
+                    </div>
+                    <div className='contentBox absolute'>
+                        <div className='text-xs uppercase'> [tasks done] </div>
                         <br/>
-                        <div className='ratingNumber'>+22%</div>
+                        <div className='text-3xl md:text-6xl text-center'>+22%</div>
                     </div>
                 </div>
             </div>
